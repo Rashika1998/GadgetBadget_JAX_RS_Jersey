9fae5eb0a51cb4c0f195f1caf20b7ac19b30065f
Rashika1998 <rashikamr1998@gmail.com>
2021-03-23 01:17:23 +0530

    Order feature has been implemented. Success...!

--- GadgetBadget_REST_JERSEY/src/com/gadget_badget/orders/model/OrderServlet.java
+++ GadgetBadget_REST_JERSEY/src/com/gadget_badget/orders/model/OrderServlet.java
@@ -1,5 +1,209 @@
 package com.gadget_badget.orders.model;
 
-public class OrderServlet {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
-}
+public class OrderServlet 
+{
+	
+	//A common method to connect to the DB
+	private Connection connect() 
+	{ 
+		Connection con = null; 
+		try
+		{ 
+			Class.forName("com.mysql.jdbc.Driver"); 
+			 
+			 //Provide the correct details: DBServer/DBName, username, password 
+			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/gadgetbadget_rest_jersey", "root", "issa123"); 
+		} 
+		catch (Exception e) 
+		{e.printStackTrace();} 
+		 	return con; 
+		} 
+	
+	
+	
+	
+		 public String insertOrder(String order_code, String customer_id, String customer_email, String customer_name, String order_total_amount, String card_no, String cvv_no) 
+		 { 
+			 String output = ""; 
+			 try
+			 { 
+				 Connection con = connect(); 
+				 if (con == null) 
+				 {
+					 return "Error while connecting to the database for inserting."; 
+				 } 
+			 	 	 // create a prepared statement
+				 	 String query = "INSERT INTO order_tab(`orderID`,`orderCode`,`customerID`,`customerEmail`,`customerName`,`orderTotalAmount`,`cardNo`,`cvvNo`)"
+				     + " VALUES (?, ?, ?, ?, ?, ?, ?, ?)"; 
+					 PreparedStatement preparedStmt = con.prepareStatement(query); 
+					 
+					 
+					 // binding values
+					 preparedStmt.setInt(1, 0);
+					 preparedStmt.setString(2, order_code);
+					 preparedStmt.setString(3, customer_id);
+					 preparedStmt.setString(4, customer_email);
+					 preparedStmt.setString(5, customer_name);
+					 preparedStmt.setDouble(6, Double.parseDouble(order_total_amount));
+					 preparedStmt.setString(7, card_no);
+					 preparedStmt.setString(8, cvv_no);
+					 
+				 
+					 preparedStmt.execute(); 
+					 con.close(); 
+					 output = "Order has been placed successfully..!"; 
+			 } 
+			 catch (Exception e) 
+			 { 
+				 output = "Error while inserting the order into the database..!.";
+				 System.err.println(e.getMessage());
+			 } 
+		 	return output; 
+		 } 
+		 
+		 
+		 
+		 public String readOrder() 
+		 { 
+			 String output = ""; 
+			 String boostrap_link = "<head><link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css\' integrity=\'sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm\' crossorigin=\'anonymous\'></head>";
+			 try
+			 { 
+				 Connection con = connect(); 
+				 if (con == null) 
+				 {return "Error while connecting to the database for reading."; } 
+				 // Prepare the html table to be displayed
+				 output = boostrap_link + "<table border='1' style='text-align:center'><tr>"
+				 + "<th>Order Code</th>"
+				 + "<th>Customer ID</th>" +
+				 "<th>Customer Email</th>" + 
+				 "<th>Customer Name</th>" + 
+				 "<th>Total Amount</th>" +
+				 "<th>Card No</th>" +
+				 "<th>CVV No</th>" +
+				 "<th>Update</th><th>Remove</th></tr>"; 
+			 
+				 String query = "SELECT * FROM order_tab"; 
+				 Statement stmt = con.createStatement(); 
+				 ResultSet rs = stmt.executeQuery(query); 
+				 // iterate through the rows in the result set
+				 while (rs.next()) 
+				 { 
+					 String orderID = Integer.toString(rs.getInt("orderID")); 
+					 String orderCode = rs.getString("orderCode"); 
+					 String customerID = rs.getString("customerID"); 
+					 String customerEmail = rs.getString("customerEmail"); 
+					 String customerName = rs.getString("customerName"); 
+					 String orderTotalAmount = Double.toString(rs.getDouble("orderTotalAmount")); 
+					 String cardNo = rs.getString("cardNo");
+					 String cvvNo = rs.getString("cvvNo");
+					 
+					 
+					 // Add into the html table
+					 output += "<tr><td>" + orderCode + "</td>"; 
+					 output += "<td>" + customerID + "</td>"; 
+					 output += "<td>" + customerEmail + "</td>"; 
+					 output += "<td>" + customerName + "</td>"; 
+					 output += "<td>" + orderTotalAmount + "</td>"; 
+					 output += "<td>" + cardNo + "</td>"; 
+					 output += "<td>" + cvvNo + "</td>"; 
+					 
+					 
+					 
+					 // buttons
+					 output += "<td><input name='btnUpdate' type='button' value='Update' class='btn btn-secondary'></td>"
+					 + "<td><form method='post' action='items.jsp'>"
+					 + "<input style='margin-top:15px' name='btnRemove' type='submit' value='Remove' class='btn btn-danger'>"
+					 + "<input name='orderID' type='hidden' value='" + orderID 
+					 + "'>" + "</form></td></tr>"; 
+				 } 
+				 	 con.close(); 
+				 	 // Complete the html table
+				 	 output += "</table>"; 
+			 } 
+			 catch (Exception e) 
+			 { 
+				 output = "Error while reading the order details...!"; 
+				 System.err.println(e.getMessage()); 
+			 } 
+		 	 return output; 
+		 } 
+		
+		
+		
+		public String updateOrder(String order_id, String order_code, String customer_id, String customer_email, String customer_name , String order_total_amount , String card_no , String cvv_no)
+		{ 
+			 String output = ""; 
+			 try
+			 { 
+				 Connection con = connect(); 
+				 if (con == null) 
+				 {
+					 return "Error while connecting to the database for updating."; 
+				 } 
+				 // create a prepared statement
+				 String query = "UPDATE order_tab SET orderCode=? , customerID=? , customerEmail=? , customerName=? , orderTotalAmount=? , cardNo=? , cvvNo=?  WHERE orderID=?"; 
+				 PreparedStatement preparedStmt = con.prepareStatement(query); 
+				 // binding values
+				 preparedStmt.setString(1, order_code); 
+				 preparedStmt.setString(2, customer_id); 
+				 preparedStmt.setString(3, customer_email); 
+				 preparedStmt.setString(4, customer_name); 
+				 preparedStmt.setDouble(5, Double.parseDouble(order_total_amount)); 
+				 preparedStmt.setString(6, card_no); 
+				 preparedStmt.setString(7, cvv_no); 
+				 preparedStmt.setInt(8, Integer.parseInt(order_id)); 
+				 
+				 
+				 // execute the statement
+				 preparedStmt.execute(); 
+				 con.close(); 
+				 output = "Order details have been updated successfully...!"; 
+			 } 
+			 catch (Exception e) 
+			 { 
+				 output = "Error while updating order details...!"; 
+				 System.err.println(e.getMessage()); 
+			 } 
+			 	return output; 
+			 } 
+		
+		
+		
+			 public String deleteOrder(String orderID) 
+			 { 
+				 String output = ""; 
+			 try
+			 { 
+				 Connection con = connect(); 
+			 if (con == null) 
+			 {
+				 return "Error while connecting to the database for deleting."; 
+			 } 
+			 
+			 	 // create a prepared statement
+				 String query = "DELETE FROM order_tab WHERE orderID=?"; 
+				 PreparedStatement preparedStmt = con.prepareStatement(query); 
+				 
+				 // binding values
+				 preparedStmt.setInt(1, Integer.parseInt(orderID)); 
+				 
+				 // execute the statement
+				 preparedStmt.execute(); 
+				 con.close(); 
+				 output = "Order has been deleted successfully"; 
+			 } 
+			 catch (Exception e) 
+			 { 
+				 output = "Error while deleting the order."; 
+				 System.err.println(e.getMessage()); 
+			 } 
+			 return output; 
+			 } 
+} 
--- GadgetBadget_REST_JERSEY/src/com/gadget_badget/orders/service/OrderService.java
+++ GadgetBadget_REST_JERSEY/src/com/gadget_badget/orders/service/OrderService.java
@@ -1,5 +1,101 @@
 package com.gadget_badget.orders.service;
 
-public class OrderService {
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.parser.Parser;
+
+import com.gadget_badget.orders.model.OrderServlet;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+@Path("/Orders") 
+public class OrderService 
+{
+	
+	OrderServlet orderObj = new OrderServlet(); 
+	
+	@GET
+	@Path("/") 
+	@Produces(MediaType.TEXT_HTML) 
+	public String readOrder() 
+	{ 
+		return orderObj.readOrder(); 
+	} 
+	
+	
+	@POST
+	@Path("/") 
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED) 
+	@Produces(MediaType.TEXT_PLAIN) 
+	public String insertOrder(
+		 @FormParam("orderCode") String orderCode, 
+		 @FormParam("customerID") String customerID, 
+		 @FormParam("customerEmail") String customerEmail, 
+		 @FormParam("customerName") String customerName,
+		 @FormParam("orderTotalAmount") String orderTotalAmount,
+		 @FormParam("cardNo") String cardNo,
+		 @FormParam("cvvNo") String cvvNo)
+		 
+	{ 
+		String output = orderObj.insertOrder(orderCode, customerID, customerEmail, customerName, orderTotalAmount, cardNo, cvvNo); 
+		return output; 
+	}
+	
+	
+	
+	
+	
+	
+	
+	@PUT
+	@Path("/") 
+	@Consumes(MediaType.APPLICATION_JSON) 
+	@Produces(MediaType.TEXT_PLAIN) 
+	public String updateOrder(String orderData) 
+	{ 
+		//Convert the input string to a JSON object 
+		 JsonObject orderObject = new JsonParser().parse(orderData).getAsJsonObject(); 
+		//Read the values from the JSON object
+		 String orderID = orderObject.get("orderID").getAsString(); 
+		 String orderCode = orderObject.get("orderCode").getAsString(); 
+		 String customerID = orderObject.get("customerID").getAsString(); 
+		 String customerEmail = orderObject.get("customerEmail").getAsString(); 
+		 String customerName = orderObject.get("customerName").getAsString(); 
+		 String orderTotalAmount = orderObject.get("orderTotalAmount").getAsString(); 
+		 String cardNo = orderObject.get("cardNo").getAsString(); 
+		 String cvvNo = orderObject.get("cvvNo").getAsString(); 
+		 
+		 String output = orderObj.updateOrder(orderID, orderCode, customerID, customerEmail, customerName, orderTotalAmount, cardNo, cvvNo); 
+		 return output; 
+	}
+	
+	
+	
+	
+	
+	@DELETE
+	@Path("/") 
+	@Consumes(MediaType.APPLICATION_XML) 
+	@Produces(MediaType.TEXT_PLAIN) 
+	public String deleteOrder(String orderData) 
+	{ 
+		//Convert the input string to an XML document
+		 Document doc = Jsoup.parse(orderData, "", Parser.xmlParser()); 
+		 
+		//Read the value from the element <itemID>
+		 String orderID = doc.select("orderID").text(); 
+		 String output = orderObj.deleteOrder(orderID); 
+		 return output; 
+	}
 
 }
