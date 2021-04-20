40761bcd4fd302b7fbb168ef1cf4a889471aba17
alvinz97 <it18211306@my.sliit.lk>
2021-04-20 12:03:14 +0530

    Added DB configuration method for servlet

--- GadgetBadget_REST_JERSEY/src/com/gadget_badget/orders/model/OrderServlet.java
+++ GadgetBadget_REST_JERSEY/src/com/gadget_badget/orders/model/OrderServlet.java
@@ -8,5 +8,15 @@ import java.sql.Statement;
 
 public class OrderServlet 
 {
-			 
+		//A common method to connect to the DB
+		private Connection connect()  { 
+			Connection con = null; 
+			try { 
+				Class.forName("com.mysql.jdbc.Driver"); 
+				 
+				 //Provide the correct details: DBServer/DBName, username, password 
+				con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/gadgetbadget_rest_jersey", "root", "issa123"); 
+			} catch (Exception e)  {e.printStackTrace();} 
+			 	return con; 
+		}   
 } 
