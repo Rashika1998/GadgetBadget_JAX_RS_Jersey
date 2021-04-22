e72f57ff4e022fb0ef3a6aa9e0c1399e2358c514
gavidya <it18225716@my.sliit.lk>
2021-04-22 13:35:15 +0530

    implemeneting add to cart method in CartServlet

--- GadgetBadget_REST_JERSEY/src/com/gadget_badget/cart/model/CartServlet.java
+++ GadgetBadget_REST_JERSEY/src/com/gadget_badget/cart/model/CartServlet.java
@@ -22,4 +22,27 @@ public class CartServlet
 		{e.printStackTrace();} 
 		 	return con; 
 		} 
+
+
+        public String insertCart(String cart_code, String project_code, String project_name, String project_qty , String project_unit_price , String customer_id) 
+		 { 
+			 String output = ""; 
+			 try
+			 { 
+				 Connection con = connect(); 
+				 if (con == null) 
+				 {
+					 return "Error while connecting to the database for inserting."; 
+				 } 
+
+                 
+			 } 
+			 catch (Exception e) 
+			 { 
+				 output = "Error while inserting the project into the cart..!."; 
+				 System.err.println(e.getMessage()); 
+			 } 
+		 	return output; 
+		 }
+
 } 
