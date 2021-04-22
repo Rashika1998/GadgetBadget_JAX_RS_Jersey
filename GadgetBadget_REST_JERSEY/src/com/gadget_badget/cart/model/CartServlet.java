90bdd4a4212ba823078114fff223cb7447d53d93
gavidya <it18225716@my.sliit.lk>
2021-04-22 12:52:18 +0530

    implemeneted connection method

--- GadgetBadget_REST_JERSEY/src/com/gadget_badget/cart/model/CartServlet.java
+++ GadgetBadget_REST_JERSEY/src/com/gadget_badget/cart/model/CartServlet.java
@@ -8,5 +8,18 @@ import java.sql.Statement;
 
 public class CartServlet 
 {
-	
+	private Connection connect() 
+	{ 
+		Connection con = null; 
+		try
+		{ 
+			Class.forName("com.mysql.jdbc.Driver"); 
+			 
+			 //Provide the correct details: DBServer/DBName, username, password 
+			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/gadgetbadget_rest_jersey", "root", ""); 
+		} 
+		catch (Exception e) 
+		{e.printStackTrace();} 
+		 	return con; 
+		} 
 } 
